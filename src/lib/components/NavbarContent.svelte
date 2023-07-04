9f91e8e65c92da1a3561641db3f13ef98ac5d0ab
RazielO <oraziel_a@hotmail.com>
2023-07-03 21:18:04 -0600

    Using goto instead of links

--- src/lib/components/NavbarContent.svelte
+++ src/lib/components/NavbarContent.svelte
@@ -1,15 +1,16 @@
 <script>
+	import { goto } from '$app/navigation';
 	import { Button, Divider, Menu } from '@svelteuidev/core';
 </script>
 
 <Menu>
 	<Button slot="control" variant="subtle" ripple>Menu</Button>
 	<Menu.Item disabled><b>Root Finding</b></Menu.Item>
-	<Menu.Item><a href="/rootFinding/bisection">Bisection</a></Menu.Item>
-	<Menu.Item><a href="/rootFinding/regulaFalsi">Regula Falsi</a></Menu.Item>
-	<Menu.Item><a href="/rootFinding/fixedPoint">Fixed Point</a></Menu.Item>
-	<Menu.Item><a href="/rootFinding/newtonRaphson">Newton-Raphson</a></Menu.Item>
-	<Menu.Item><a href="/rootFinding/secant">Secant</a></Menu.Item>
+	<Menu.Item on:click={() => goto('/rootFinding/bisection')}>Bisection</Menu.Item>
+	<Menu.Item on:click={() => goto('/rootFinding/regulaFalsi')}>Regula Falsi</Menu.Item>
+	<Menu.Item on:click={() => goto('/rootFinding/fixedPoint')}>Fixed Point</Menu.Item>
+	<Menu.Item on:click={() => goto('/rootFinding/newtonRaphson')}>Newton-Raphson</Menu.Item>
+	<Menu.Item on:click={() => goto('/rootFinding/secant')}>Secant</Menu.Item>
 	<Menu.Item disabled><b>Equation Solving</b></Menu.Item>
-	<Menu.Item><a href="/equationSolving/gauss">Gauss</a></Menu.Item>
+	<Menu.Item on:click={() => goto('/equationSolving/gauss')}>Gauss</Menu.Item>
 </Menu>
